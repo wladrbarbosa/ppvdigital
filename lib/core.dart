377fcc5af10eb5c31e455069030b570d66d21023
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2026-07-28 05:27:44 -0300

    Atualizada versão

--- lib/core.dart
+++ lib/core.dart
@@ -144,5 +144,5 @@ class Core {
   }
 
   static GlobalKey<TarefasPageState> globalKey = GlobalKey<TarefasPageState>();
-  static const String appVersion = 'b0.21.0+1';
+  static const String appVersion = 'b0.21.1+1';
 }
